7c317804940ed11a58456528067361d4c6959ae9
J. Hinchliffe <jjh413@student.bham.ac.uk>
2025-09-17 13:41:27 +0100

    Fix compilation errors in bindings

--- src/root.zig
+++ src/root.zig
@@ -47,52 +47,43 @@ pub const ZstdError = error{
 /// Called to convert our C error codes to Zig equivalents.
 fn toError(self: c.ZSTD_ErrorCode) ZstdError {
     return switch (self) {
-        c.ZSTD_error_no_error => .NoError,
-        c.ZSTD_error_GENERIC => .Generic,
-        c.ZSTD_error_prefix_unknown => .PrefixUnknown,
-        c.ZSTD_error_version_unsupported => .VersionUnsupported,
-        c.ZSTD_error_frameParameter_unsupported => .FrameParameterUnsupported,
-        c.ZSTD_error_frameParameter_windowTooLarge => .FrameParameterWindowTooLarge,
-        c.ZSTD_error_corruption_detected => .CorruptionDetected,
-        c.ZSTD_error_checksum_wrong => .ChecksumWrong,
-        c.ZSTD_error_literals_headerWrong => .LiteralsHeaderWrong,
-        c.ZSTD_error_dictionary_corrupted => .DictionaryCorrupted,
-        c.ZSTD_error_dictionary_wrong => .DictionaryWrong,
-        c.ZSTD_error_dictionaryCreation_failed => .DictionaryCreationFailed,
-        c.ZSTD_error_parameter_unsupported => .ParameterUnsupported,
-        c.ZSTD_error_parameter_combination_unsupported => .ParameterCombinationUnsupported,
-        c.ZSTD_error_parameter_outOfBound => .ParameterOutOfBound,
-        c.ZSTD_error_tableLog_tooLarge => .TableLogTooLarge,
-        c.ZSTD_error_maxSymbolValue_tooLarge => .MaxSymbolValueTooLarge,
-        c.ZSTD_error_maxSymbolValue_tooSmall => .MaxSymbolValueTooSmall,
-        c.ZSTD_error_cannotProduce_uncompressedBlock => .CannotProduceUncompressedBlock,
-        c.ZSTD_error_stabilityCondition_notRespected => .StabilityConditionNotRespected,
-        c.ZSTD_error_stage_wrong => .StageWrong,
-        c.ZSTD_error_init_missing => .InitMissing,
-        c.ZSTD_error_memory_allocation => .MemoryAllocation,
-        c.ZSTD_error_workSpace_tooSmall => .WorkspaceTooSmall,
-        c.ZSTD_error_dstSize_tooSmall => .DstSizeTooSmall,
-        c.ZSTD_error_srcSize_wrong => .SrcSizeWrong,
-        c.ZSTD_error_dstBuffer_null => .DstBufferNull,
-        c.ZSTD_error_noForwardProgress_destFull => .NoForwardProgressDestFull,
-        c.ZSTD_error_noForwardProgress_inputEmpty => .NoForwardProgressInputEmpty,
-        c.ZSTD_error_frameIndex_tooLarge => .FrameIndexTooLarge,
-        c.ZSTD_error_seekableIO => .SeekableIo,
-        c.ZSTD_error_dstBuffer_wrong => .DstBufferWrong,
-        c.ZSTD_error_srcBuffer_wrong => .SrcBufferWrong,
-        c.ZSTD_error_sequenceProducer_failed => .SequenceProducerFailed,
-        c.ZSTD_error_externalSequences_invalid => .ExternalSequencesInvalid,
-        c.ZSTD_error_maxCode => .MaxCode,
-    };
-}
-
-/// Compile errors when we don't pass in a Zig error, or we haven't expressed
-/// the error in the Zstd error set. This is just here to coerce errors into
-/// Zstd errors.
-fn toZstdError(err: anyerror) ZstdError {
-    return switch (err) {
-        error.OutOfMemory => ZstdError.MemoryAllocation,
-        else => @compileError("Unhandled error: " ++ @errorName(err) ++ "!"),
+        c.ZSTD_error_no_error => ZstdError.NoError,
+        c.ZSTD_error_GENERIC => ZstdError.Generic,
+        c.ZSTD_error_prefix_unknown => ZstdError.PrefixUnknown,
+        c.ZSTD_error_version_unsupported => ZstdError.VersionUnsupported,
+        c.ZSTD_error_frameParameter_unsupported => ZstdError.FrameParameterUnsupported,
+        c.ZSTD_error_frameParameter_windowTooLarge => ZstdError.FrameParameterWindowTooLarge,
+        c.ZSTD_error_corruption_detected => ZstdError.CorruptionDetected,
+        c.ZSTD_error_checksum_wrong => ZstdError.ChecksumWrong,
+        c.ZSTD_error_literals_headerWrong => ZstdError.LiteralsHeaderWrong,
+        c.ZSTD_error_dictionary_corrupted => ZstdError.DictionaryCorrupted,
+        c.ZSTD_error_dictionary_wrong => ZstdError.DictionaryWrong,
+        c.ZSTD_error_dictionaryCreation_failed => ZstdError.DictionaryCreationFailed,
+        c.ZSTD_error_parameter_unsupported => ZstdError.ParameterUnsupported,
+        c.ZSTD_error_parameter_combination_unsupported => ZstdError.ParameterCombinationUnsupported,
+        c.ZSTD_error_parameter_outOfBound => ZstdError.ParameterOutOfBound,
+        c.ZSTD_error_tableLog_tooLarge => ZstdError.TableLogTooLarge,
+        c.ZSTD_error_maxSymbolValue_tooLarge => ZstdError.MaxSymbolValueTooLarge,
+        c.ZSTD_error_maxSymbolValue_tooSmall => ZstdError.MaxSymbolValueTooSmall,
+        c.ZSTD_error_cannotProduce_uncompressedBlock => ZstdError.CannotProduceUncompressedBlock,
+        c.ZSTD_error_stabilityCondition_notRespected => ZstdError.StabilityConditionNotRespected,
+        c.ZSTD_error_stage_wrong => ZstdError.StageWrong,
+        c.ZSTD_error_init_missing => ZstdError.InitMissing,
+        c.ZSTD_error_memory_allocation => ZstdError.MemoryAllocation,
+        c.ZSTD_error_workSpace_tooSmall => ZstdError.WorkspaceTooSmall,
+        c.ZSTD_error_dstSize_tooSmall => ZstdError.DstSizeTooSmall,
+        c.ZSTD_error_srcSize_wrong => ZstdError.SrcSizeWrong,
+        c.ZSTD_error_dstBuffer_null => ZstdError.DstBufferNull,
+        c.ZSTD_error_noForwardProgress_destFull => ZstdError.NoForwardProgressDestFull,
+        c.ZSTD_error_noForwardProgress_inputEmpty => ZstdError.NoForwardProgressInputEmpty,
+        c.ZSTD_error_frameIndex_tooLarge => ZstdError.FrameIndexTooLarge,
+        c.ZSTD_error_seekableIO => ZstdError.SeekableIo,
+        c.ZSTD_error_dstBuffer_wrong => ZstdError.DstBufferWrong,
+        c.ZSTD_error_srcBuffer_wrong => ZstdError.SrcBufferWrong,
+        c.ZSTD_error_sequenceProducer_failed => ZstdError.SequenceProducerFailed,
+        c.ZSTD_error_externalSequences_invalid => ZstdError.ExternalSequencesInvalid,
+        c.ZSTD_error_maxCode => ZstdError.MaxCode,
+        else => unreachable,
     };
 }
 
@@ -129,7 +120,7 @@ pub const Diagnostics = struct {
             return writer.print("(no errors reported)", .{});
 
         const s = &self.source_info;
-        try writer.print("In {s}/{s}:{d}:{d} (in {s})", .{
+        try writer.print("In {s}/{s}:{d}:{d} (in {s})\n", .{
             s.module,
             s.file,
             s.line,
@@ -201,9 +192,13 @@ pub const Frame = struct {
         }
 
         const decompressed = alloc.alloc(u8, bound) catch |err| {
-            self.diag.set("Ran out of memory allocating decompressed data buffer.");
-
-            return toZstdError(err);
+            switch (err) {
+                error.OutOfMemory => {
+                    self.diag.set("Ran out of memory allocating decompressed data buffer.");
+                    return ZstdError.MemoryAllocation;
+                },
+                else => unreachable,
+            }
         };
 
         const bytes = c.ZSTD_decompress(
@@ -247,9 +242,13 @@ pub fn compress(
     }
 
     const compressed = alloc.alloc(u8, bound) catch |err| {
-        self.diag.set("Ran out of memory allocating compressed data buffer.");
-
-        return toZstdError(err);
+        switch (err) {
+            error.OutOfMemory => {
+                self.diag.set("Ran out of memory allocating compressed data buffer.");
+                return ZstdError.MemoryAllocation;
+            },
+            else => unreachable,
+        }
     };
 
     errdefer alloc.free(compressed);
@@ -275,15 +274,14 @@ pub fn compress(
 }
 
 inline fn getZstdErr(code: usize) ZstdError {
-    const enum_err = @as(c.ZSTD_ErrorCode, @bitCast(code));
+    const enum_err = @as(c.ZSTD_ErrorCode, @truncate(code));
 
     return toError(enum_err);
 }
 
-inline fn getZstdErrStr(code: usize) [:0]u8 {
-    return c.ZSTD_getErrorString(
-        @intCast(code),
-    );
+inline fn getZstdErrStr(code: usize) [:0]const u8 {
+    const c_str = c.ZSTD_getErrorString(@intCast(code));
+    return std.mem.span(c_str);
 }
 
 const TEST_DATA = @embedFile("root.zig");
